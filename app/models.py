acefe8488e8de28606f4122e4acfe5d75101fcb1
LeTravis <zorrysorry.not@gmail.com>
2024-10-15 15:29:13 +0800

    #Today i worked on doing the calculate per game average using a list and looping through it but it didnt work because the average number never gets put back into the self.points, which made it so it never saved the average. #next i will finish this method and start with views.

--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
 
-class Stats(Model):
+class Player(Model):
     player = StringField()
     position = StringField()
-    points = IntegerField()
-    rebounds = IntegerField()
-    assists = IntegerField()
-    blocks = IntegerField()
-    steals = IntegerField()
+    points = FloatField()
+    rebounds = FloatField()
+    assists = FloatField()
+    blocks = FloatField()
+    steals = FloatField()
     shotstaken = IntegerField()
     shotsmade = IntegerField()
     threestaken = IntegerField()
@@ -40,16 +40,34 @@ class Stats(Model):
             'matches_played': self.matchesplayed,
             'shots_taken': self.shotstaken,
             'shots_made': self.shotsmade,
-            'FG%': self.calculate_fg%(),
-            '3PFG%': self.calculate_3pfg%(),
+            'FG%': self.calculate_fg_percent(),
+            '3PFG%': self.calculate_3pfg_percent(),
             'Plus_minus': self.plusminus
         }
 
 
-    def calculate_fg%(self):
+    def calculate_fg_percent(self):
         return self.shotsmade/self.shotstaken * 100
     
     
-    def calculate_3pfg%(self):
-            return self.threesmade/self.threestaken * 100
+    def calculate_3pfg_percent(self):
+        return self.threesmade/self.threestaken * 100
 
+    def add_stats(self, points, assists, rebounds, blocks, steals):
+        self.points += points
+        self.assists += assists
+        self.rebounds += rebounds
+        self.blocks += blocks
+        self.steals += steals
+        self.save()
+
+    def calculate_per_game_avg(self):
+        statslist = [self.points, self.assists, self.rebounds, self.blocks, self.steals]
+        for stats in statslist:
+            print(stats)
+            stats = stats / self.matchesplayed
+            stats = round(stats, 1)
+            print(stats)
+            self.save()
+            print(self.points)
+        
