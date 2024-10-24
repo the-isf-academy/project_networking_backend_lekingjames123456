f96b532f0b9f5e56c371d94f32116ea063c58370
LeTravis <zorrysorry.not@gmail.com>
2024-10-24 13:11:02 +0800

    today i finished my product, i was super proud of the complex code i used and how unique the api is compared to the labs we did in class.

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
 
-class Player(Model):
+class Player(Model): #the model and what it contains
     player = StringField()
     position = StringField()
     points = FloatField()
@@ -16,10 +16,9 @@ class Player(Model):
     threesmade = IntegerField()
     matchesplayed = IntegerField()
     plusminus = IntegerField()
-    mvp = IntegerField()
     active = BooleanField()
 
-    def json_response(self):
+    def json_response(self): #this is what the user sees after they trigger a json response from the endpoints
         return {
             'player': self.player,
             'position': self.position,
@@ -29,33 +28,32 @@ class Player(Model):
             'rebounds': self.rebounds,
             'steals': self.steals,
             'blocks': self.blocks,
-            'MVPs': self.mvp,
             'active': self.active
         }
     
-    def json_response_advanced(self):
+    def json_response_advanced(self): #this is what the user sees after they trigger a json response from the endpoints
         return{
             'player': self.player,
             'position': self.position,
             'matches_played': self.matchesplayed,
             'shots_taken': self.shotstaken,
             'shots_made': self.shotsmade,
-            'threes_taken': self.threestaken
-            'threes_made': self.threesmade
+            'threes_taken': self.threestaken,
+            'threes_made': self.threesmade,
             'FG%': self.calculate_fg_percent(),
             '3PFG%': self.calculate_3pfg_percent(),
             'Plus_minus': self.plusminus
         }
 
 
-    def calculate_fg_percent(self):
+    def calculate_fg_percent(self): #takes the shots made and divides it by the shots taken, then multiplies it by 100 to make it a percentage
         return self.shotsmade/self.shotstaken * 100
     
     
-    def calculate_3pfg_percent(self):
+    def calculate_3pfg_percent(self): #takes the three pointers made and divides it by the three pointers taken, then multiplies it by 100 to make it a percentage
         return self.threesmade/self.threestaken * 100
 
-    def add_stats(self, points, assists, rebounds, blocks, steals):
+    def add_stats(self, points, assists, rebounds, blocks, steals): #takes the stats the user inputs, then adds it to the existing stats
         self.points += points
         self.assists += assists
         self.rebounds += rebounds
@@ -64,16 +62,19 @@ class Player(Model):
         self.matchesplayed += 1
         self.save()
 
-    def add_stats_advanced(self, shotstaken, threestaken, shotsmade, threesmade, plusminus):
+    def add_stats_advanced(self, shotstaken, threestaken, plusminus, shotsmade, threesmade): #takes the advanced stats the user inputs, then adds it to the existing advanced stats
         self.shotstaken += shotstaken
         self.threestaken += threestaken
+        self.plusminus += plusminus
         self.shotsmade += shotsmade
         self.threesmade += threesmade
-        self.plusminus += plusminus
+        self.matchesplayed += 1
+        
+        self.save()
 
 
 
-    # def calculate_per_game_avg(self):
+    # def calculate_per_game_avg(self):                             #didn't work, but worth a try
     #     statslist = [self.points, self.assists, self.rebounds, self.blocks, self.steals]
     #     for stats in statslist:
     #         print(stats)
@@ -83,11 +84,48 @@ class Player(Model):
     #         self.save()
     #         print(self.points)
 
-    def calculate_per_game_avg(self):
-        self.points = self.points / self.matchesplayed
-        self.assists = self.assists / self.matchesplayed
-        self.rebounds = self.rebounds / self.matchesplayed
-        self.blocks = self.blocks / self.matchesplayed
-        self.steals = self.steals / self.matchesplayed
+    def calculate_per_game_avg(self): #calculates their per game averages by taking how many points they have scored in total and divides it by how many matches they have played
+        if self.points > 0: #makes sure that it's not dividing 0 by 0
+            self.points = self.points / self.matchesplayed
+            self.points = round(self.points, 2) #rounds it to 2 decimal places
+        else:
+            self.points = 0
+
+        if self.assists > 0: #makes sure that it's not dividing 0 by 0
+            self.assists = self.assists / self.matchesplayed
+            self.assists = round(self.assists, 2) #rounds it to 2 decimal places
+        else:
+            self.assists = 0
+
+        if self.rebounds > 0: #makes sure that it's not dividing 0 by 0
+            self.rebounds = self.rebounds / self.matchesplayed
+            self.rebounds = round(self.rebounds, 2) #rounds it to 2 decimal places
+        else:
+            self.rebounds = 0
+
+        if self.blocks > 0: #makes sure that it's not dividing 0 by 0
+            self.blocks = self.blocks / self.matchesplayed
+            self.blocks = round(self.blocks, 2) #rounds it to 2 decimal places
+        else:
+            self.blocks = 0
+
+        if self.steals > 0: #makes sure that it's not dividing 0 by 0       
+            self.steals = self.steals / self.matchesplayed
+            self.steals = round(self.steals, 2) #rounds it to 2 decimal places
+        else:
+            self.steals = 0
+
         self.save()
         
+
+    def player_inactive(self): #sets a player inactive
+        if self.active == True:
+            self.active = False
+
+        self.save()
+
+    def player_active(self): #sets a player active after they are made inactive
+        if self.active == False:
+            self.active = True
+
+        self.save()
--- app/views.py
+++ app/views.py
@@ -4,21 +4,24 @@ from banjo.urls import route_get, route_post
 from .models import Player
 from settings import BASE_URL
 
-@route_post(BASE_URL + 'new', args={'player_name':str, 'player_position':str})
+@route_post(BASE_URL + 'new', args={'player':str, 'position':str}) #creates a new player
 def newplayer(args):
-    newplayer = Player(
-        player = args['player_name'],
-        position = args['player_position'],
-        active = True
-    )
+    if Player.objects.filter(player=args['player'], active = True).exists():
+        return {'error': 'player is already in the API'}
+    else:
+        newplayer = Player(  #creates the new player with their name and position
+            player = args['player'],
+            position = args['position'],
+            active = True
+        )
+        
+        newplayer.save()
 
-    newplayer.save()
+        return {'player info': newplayer.json_response()}
 
-    return {'player info': newplayer.json_response()}
-
-@route_get(BASE_URL + 'stats', args={'player':str})
+@route_get(BASE_URL + 'stats', args={'player':str}) #gets the stats of a player
 def playerstats(args):
-    if Player.objects.filter(player=args['player']).exists():
+    if Player.objects.filter(player=args['player'], active = True).exists():
         playerstats = Player.objects.get(player=args['player'])
         playerstats.calculate_per_game_avg()
 
@@ -29,8 +32,8 @@ def playerstats(args):
 
 @route_get(BASE_URL + 'stats/advanced', args={'player':str})
 def advancedstats(args):
-    if player.objects.filter(player=args['player_name']).exists():
-        playerstats = Player.objects.get(player=args['player'])
+    if Player.objects.filter(player=args['player'], active = True).exists():
+        advancedstats = Player.objects.get(player=args['player'])
 
         return {'advanced info': advancedstats.json_response_advanced()}
 
@@ -48,7 +51,7 @@ def allplayers(args):
         'center': []
     }
 
-    for player in Player.objects.all():
+    for player in Player.objects.filter(active = True):
         if player.position == 'point guard':
             allplayers['point guard'].append(player.player)
         if player.position == 'shooting guard':
@@ -64,20 +67,20 @@ def allplayers(args):
 
 @route_get(BASE_URL + 'leaders')
 def statleader(args):
-    pointleader = Player.objects.order_by('-points').first()
-    pointleader = pointleader.player
+    pointleader = Player.objects.filter(active = True).order_by('-points').first()
+    pointleader = pointleader.player, pointleader.points
 
-    assistleader = Player.objects.order_by('-assists').first()
-    assistleader = assistleader.player
+    assistleader = Player.objects.filter(active = True).order_by('-assists').first()
+    assistleader = assistleader.player, assistleader.assists
 
-    reboundleader = Player.objects.order_by('-rebounds').first()
-    reboundleader = reboundleader.player
+    reboundleader = Player.objects.filter(active = True).order_by('-rebounds').first()
+    reboundleader = reboundleader.player, reboundleader.rebounds
 
-    blockleader = Player.objects.order_by('-blocks').first()
-    blockleader = blockleader.player
+    blockleader = Player.objects.filter(active = True).order_by('-blocks').first()
+    blockleader = blockleader.player, blockleader.blocks
         
-    stealleader = Player.objects.order_by('-steals').first()
-    stealleader = stealleader.player
+    stealleader = Player.objects.filter(active = True).order_by('-steals').first()
+    stealleader = stealleader.player, stealleader.steals
 
     statleaders = {
         'points': pointleader,
@@ -89,9 +92,9 @@ def statleader(args):
 
     return {'leaders': statleaders}
 
-@route_get(BASE_URL + 'input_stats', args={'player':str, 'points':float, 'assists':float, 'rebounds':float, 'blocks':float, 'steals':float})
+@route_post(BASE_URL + 'input_stats', args={'player':str, 'points':float, 'assists':float, 'rebounds':float, 'blocks':float, 'steals':float})
 def addstats(args):
-    if Player.objects.filter(player=args['player']).exists():
+    if Player.objects.filter(player=args['player'], active = True).exists():
         addstats = Player.objects.get(player=args['player'])
         points_input = args['points']
         assists_input = args['assists']
@@ -105,3 +108,38 @@ def addstats(args):
     else:
         return {'error': 'no player exists'}
 
+@route_post(BASE_URL + 'input_stats_advanced', args={'player':str, 'shotstaken':int, 'threestaken':int, 'plusminus':int, 'shotsmade':int, 'threesmade':int})
+def addadvancedstats(args):
+    if Player.objects.filter(player=args['player'], active = True).exists():
+        addadvancedstats = Player.objects.get(player=args['player'])
+        shots_taken = args['shotstaken']
+        threes_taken = args['threestaken']
+        plusminus_player = args['plusminus']
+        shots_made = args['shotsmade']
+        threes_made = args['threesmade']
+        addadvancedstats.add_stats_advanced(shots_taken, threes_taken, plusminus_player, shots_made, threes_made)
+
+        return {'success': 'stats successfully added"'}
+
+    else:
+        return {'error': 'no player exists'}
+
+@route_post (BASE_URL + 'set_inactive', args={'player':str}) #sets an active player inactive
+def playerinactive(args):
+    if Player.objects.filter(player=args['player'], active = True).exists():
+        playerinactive = Player.objects.get(player=args['player'])
+        playerinactive.player_inactive()
+        return {'success': 'player has been successfully deleted'}
+    
+    else:
+        return {'error': 'no player exists'}
+
+@route_post (BASE_URL + 'set_active', args={'player':str}) #sets an previously inactive player active
+def playeractive(args):
+    if Player.objects.filter(player=args['player'], active = False).exists():
+        playeractive = Player.objects.get(player=args['player'])
+        playeractive.player_active()
+        return {'success': 'player has been successfully restored'}
+        
+    else:
+        return {'error': 'no player exists or the player is already active'}
